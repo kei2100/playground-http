bba5065574d865c80735515dd06852d98d31a47f
kei2100 <kei.arima.2100@gmail.com>
2018-06-08 10:30:26 +0900

    Add redirect-to-app-url

---
+++ redirect-to-app-url/redirect-to-app-url.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const http = require('http');
+const addr  = '127.0.0.1';
+const port = 8080;
+
+console.log(`start listening on ${addr}:${port}`);
+console.log(`try 'open http://${addr}:${port}'`)
+
+http.createServer((req, res) => {
+  res.writeHead(302, {'Location': 'dict://programming'});
+  res.end();
+}).listen(port, addr);
+

56f1f55781f36681973a67d8a40b2d7d9f641617
kei2100 <kei.arima.2100@gmail.com>
2018-06-20 23:21:39 +0900

    Fix path

--- redirect-to-app-url/redirect-to-app-url.js
+++ redirect-to-app-url/redirect-to-app-url.js
@@ -8,7 +8,7 @@ console.log(`start listening on ${addr}:${port}`);
 console.log(`try 'open http://${addr}:${port}'`)
 
 http.createServer((req, res) => {
-  res.writeHead(302, {'Location': 'dict://programming'});
+  res.writeHead(302, {'Location': 'dict:/biriyani'});
   res.end();
 }).listen(port, addr);
 
